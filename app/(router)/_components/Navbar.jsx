63d2f65bc106f97d19c7be999ec832c89252395e
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-05-07 15:27:30 +0100

    update logo

--- app/(router)/_components/Navbar.jsx
+++ app/(router)/_components/Navbar.jsx
@@ -16,13 +16,13 @@ export default function Navbar() {
 
   return (
     <nav className='flex items-center justify-between py-2 px-5 lg:px-20 shadow-md'>
-        <Image src="/logo.jpg" width={60} height={60} alt="logo"/>
+        <Image src="/logo.png" width={80} height={60} alt="logo"/>
 
         <FaAlignJustify className='md:hidden'/>
 
         <ul className='flex flex-col md:flex-row items-center gap-3'>
             {menu.map(item => (
-                <Link key={item.url} href={item.url} className={`flex gap-3 items-center py-2 px-3 text-[18px] text-slate-500 cursor-pointer hover:bg-purple-700 hover:text-slate-200 ease-in-out duration-300 rounded-md ${path === item.url && "bg-purple-700 text-slate-100"}`}>
+                <Link key={item.url} href={item.url} className={`${path === item.url && "bg-purple-700 text-white"} flex gap-3 items-center py-2 px-3 text-[18px] text-slate-700 cursor-pointer hover:bg-purple-700 hover:text-slate-200 ease-in-out duration-300 rounded-md`}>
                     {item.label}
                 </Link>
             ))}
