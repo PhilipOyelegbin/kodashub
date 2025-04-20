8c6ff171ca533d1f8962208a13bd103e5986b2e8
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-04-20 11:41:31 +0100

    update: updated for OS change

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,22 @@ app.use("/", authRoutes);
 app.use("/", passwordResetRoutes);
 
 // Swagger setup
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerdocs));
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerdocs, {
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  })
+);
 
 // error route
 app.all("*", (req, res) => {
